53951f2dafaabd2b45db5526317a62e7607028ca
otakustay <otakustay@gmail.com>
2024-12-02 20:41:48 +0800

    chore: refine some lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,21 @@ export default ts.config(
             '@typescript-eslint/no-unsafe-assignment': 'off',
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/no-empty-function': 'off',
+            '@typescript-eslint/array-type': [
+                'error',
+                {
+                    default: 'array-simple',
+                    readonly: 'array-simple',
+                },
+            ],
+            '@typescript-eslint/restrict-template-expressions': 'off',
+            '@typescript-eslint/no-confusing-void-expression': [
+                'error',
+                {
+                    ignoreArrowShorthand: true,
+                    ignoreVoidOperator: true,
+                },
+            ],
         },
     }
 );
